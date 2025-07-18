231cdb5bcb616b1cbbe0433821bf2ceb132ee9bd
ambuj21 <ambuj20maurya@gmail.com>
2025-07-18 07:54:49 +0530

    update compare.java

---
+++ practice/compare.java
@@ -0,0 +1,15 @@
+public class compare{
+    public static void main(String[] args) {
+        int n=15;
+        if(n>0){
+        System.out.println("Positive");
+    }
+    else if(n==0){
+        System.out.println("Zero");
+    }
+    else{
+        System.out.println("Negative");
+    }
+    }
+
+}
